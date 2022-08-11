b94a12cc98101cfc21c412af9512e73d2903cedd
Matthew Foreman <71850564+4mattX@users.noreply.github.com>
2022-08-10 19:40:50 -0500

    Delete svelte-kit directory

--- svelte-kit/output/server/nodes/1.js
+++
@@ -1,7 +0,0 @@
-import * as module from '../entries/fallbacks/error.svelte.js';
-
-export { module };
-export const index = 1;
-export const file = 'error.svelte-e6e33c10.js';
-export const imports = ["error.svelte-e6e33c10.js","chunks/index-9337fb22.js"];
-export const stylesheets = [];
